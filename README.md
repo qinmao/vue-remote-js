c6861e5af29b83477e261dfcb3ddb078253e836a
qm <1642903202@qq.com>
2019-07-25 21:27:51 +0800

    2.0.0

--- README.md
+++ README.md
@@ -1,27 +1,40 @@
 # v-remote-js
 [![Build Status](https://travis-ci.org/qinmao/vue-remote-js.svg?branch=master)](https://travis-ci.org/qinmao/vue-remote-js)
 
-引入远程脚本，脚本加载完成后执行回调
+在vue 单页应用中，引入远程js 和css 来避免全局引入的问题，
+不支持IE
 ## Install
 ```shell
 npm install v-remote-js  -S
 ```
 ## Quick Start
 ``` javascript
-在main.js 注册插件 使其成为一个全局组件
+在 main.js 注册插件 使其成为一个全局组件
 import Vue from 'vue'
 import vRometeJs from 'v-remote-js'
 Vue.use(vRometeJs)
-// 之后在每个xx.vue 文件中 写法如下
- <remote-js src="http://xxx.js" @scriptLoad="callbackFn"></remote-js>
- 
+```
+
+```html
+ <!-- 之后在每个xx.vue 文件中 写法如下 -->
+<remote-css :hrefs="hrefs"></remote-css>
+<remote-js :srcArr="srcArr" @scriptLoad="callbackFn"></remote-js>
+ ```
+ ```js
+ // 支持加载多个远程资源，加载多个js 注意依赖顺序
  export default {
+     data(){
+         return {
+            hrefs:['http://xxx.css'],
+            srcArr:['http://xxxx1.js','http://xxxx2.js']
+         }
+    }
     methods:{
+        // 远程js 加载完执行的回调
         callbackFn(){
         // ...写你的逻辑
         }  
     }
  }
-   
 ``` 
 
--- index.js
+++ index.js
@@ -2,40 +2,80 @@
 (function () {
     let vRometeJs = {}
     vRometeJs.install = function (Vue) {
-        Vue.component('remote-js', {
-            render(createElement) {
-                return createElement('script', {
-                    attrs: {
-                        type: 'text/javascript',
-                        src: this.src
-                    }
-                });
-            },
+        Vue.component('remote-css', {
             props: {
-                src: {
-                    type: String,
-                    required: true,
-                },
-            },
-            mounted() {
-                if (this.$el.readyState) {
-                    //IE
-                    this.$el.onreadystatechange = () => {
-                        if (
-                            this.$el.readyState == "loaded" ||
-                            this.$el.readyState == "complete"
-                        ) {
-                            this.$el.onreadystatechange = null;
-                            this.$emit('scriptLoad')
-                        }
-                    };
-                } else {
-                    //Others
-                    this.$el.onload = () => {
-                        this.$emit('scriptLoad')
-                    }
+                hrefs: {
+                    type: Array,
+                    required: true
                 }
             },
+            data() {
+                return {
+                    linkNodes: []
+                }
+            },
+            render(createElement) {
+                return createElement('div')
+            },
+            created() {
+                this.head = document.head || document.getElementsByTagName('head')[0]
+                this.hrefs.forEach(href => {
+                    let link = document.createElement('link')
+                    link.href = href
+                    link.rel = 'stylesheet'
+                    this.linkNodes.push(link)
+                    this.head.appendChild(link)
+                })
+            },
+            destroyed() {
+                this.linkNodes.forEach(element => {
+                    this.head.removeChild(element)
+                });
+            }
+        })
+        Vue.component('remote-js', {
+            data() {
+                return {
+                    scriptNodes: []
+                }
+            },
+            props: {
+                srcArr: {
+                    type: Array,
+                    required: true
+                }
+            },
+            render(createElement) {
+                return createElement('div')
+            },
+            created() {
+                this.head = document.head || document.getElementsByTagName('head')[0]
+                this.index = 0
+                this.createScript(this.srcArr[this.index])
+            },
+            destroyed() {
+                this.scriptNodes.forEach(element => {
+                    this.head.removeChild(element)
+                });
+            },
+            methods: {
+                createScript(src) {
+                    let script = document.createElement('script')
+                    script.src = src
+                    script.type = 'text/javascript'
+                    this.scriptNodes.push(script)
+                    this.head.appendChild(script)
+                    script.onload = () => {
+                        if (this.index < this.srcArr.length - 1) {
+                            this.index += 1
+                            this.createScript(this.srcArr[this.index])
+                        } else {
+                            this.$emit('scriptLoad')
+                        }
+
+                    }
+                }
+            }
         })
     }
     if (typeof exports === 'object') {
